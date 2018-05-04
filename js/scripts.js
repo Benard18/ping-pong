2d21b2a8f1e1b8939278f2240f1404867bbec01d
Benard18 <benkaranja43@gmail.com>
2018-05-04 14:52:32 +0300

    add js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,30 @@
 ///Business logic Interface
-var pingpong = function(number){
-  if(number % 3 === 0){
-    return "ping";
+
+    var result;
+   function condor (){
+    for (var index = 0; index <= number.length;index ++  ){
+  if(index % 3 == 0){
+    $("ul#dry").append("<li>"+ 'ping' +"</li>");
   }
-  if else(number % 5 === 0){
-    return "pong";
-  }
+   else if (index % 5 == 0) {
+     $("ul#dry").append("<li>"+ 'pong' +"</li>");
 }
+  else if(index % 15 == 0){
+  $("ul#dry").append("<li>"+ 'ping pong' +"</li>");
+  }
+else {
+  $("ul#dry").append("<li>"+ number.tostring() +"</li>");
+}
+}
+}
+///User logic Interface///
+$(document).ready(function() {
+    $("form").submit(function(event) {
+      $("#dry").text('');
+     result = $("input#number2").val();
+      condor ();
+      $("#dry").show(result);
+      $("input#number2").val("");
+      event.preventDefault();
+    });
+  });
